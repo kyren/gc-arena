909cb88259b5641f28a5ca2eafde2a49f6c2a991
Aaron Hill <aa1ronham@gmail.com>
2022-11-13 13:17:19 -0600

    Make Context pub(crate)

--- src/gc-arena/src/lib.rs
+++ src/gc-arena/src/lib.rs
@@ -21,7 +21,7 @@ mod types;
 pub use self::{
     arena::{rootless_arena, Arena, ArenaParameters, RootProvider},
     collect::Collect,
-    context::{CollectionContext, Context, MutationContext},
+    context::{CollectionContext, MutationContext},
     gc::Gc,
     gc_cell::GcCell,
     no_drop::MustNotImplDrop,
