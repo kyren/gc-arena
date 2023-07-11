848b65dcdada4b547c31819434e5d3ee44a73bce
kyren <kyren@kyju.org>
2023-07-11 11:29:18 -0400

    sprinkle inline liberally

--- src/gc-arena/src/metrics.rs
+++ src/gc-arena/src/metrics.rs
@@ -12,6 +12,7 @@ pub struct Pacing {
 /// Creates a default `Pacing` with `pause_factor` set to 0.5, `timing_factor` set to 1.5,
 /// and `min_sleep` set to 4096.
 impl Default for Pacing {
+    #[inline]
     fn default() -> Pacing {
         const PAUSE_FACTOR: f64 = 0.5;
         const TIMING_FACTOR: f64 = 1.5;
@@ -30,6 +31,7 @@ impl Pacing {
     /// retained size> * pause_multiplier` before beginning a new collection. Must be >= 0.0,
     /// setting this to 0.0 causes the collector to never sleep longer than `min_sleep` before
     /// beginning a new collection.
+    #[inline]
     pub fn set_pause_factor(mut self, pause_factor: f64) -> Pacing {
         assert!(pause_factor >= 0.0);
         self.pause_factor = pause_factor;
@@ -41,6 +43,7 @@ impl Pacing {
     /// when the arena has 100KB live data, and the timing_multiplier is 1.0, the collector should
     /// finish its final phase of this collection after another 100KB has been allocated. Must be >=
     /// 0.0, setting this to 0.0 causes the collector to behave like a stop-the-world collector.
+    #[inline]
     pub fn set_timing_factor(mut self, timing_factor: f64) -> Pacing {
         assert!(timing_factor >= 0.0);
         self.timing_factor = timing_factor;
@@ -49,6 +52,7 @@ impl Pacing {
 
     /// The minimum allocation amount during sleep before the arena starts collecting again. This is
     /// mostly useful when the heap is very small to prevent rapidly restarting collections.
+    #[inline]
     pub fn set_min_sleep(mut self, min_sleep: usize) -> Pacing {
         self.min_sleep = min_sleep;
         self
@@ -87,12 +91,14 @@ impl Metrics {
     ///
     /// The factors that affect the gc pause time will not take effect until the start of the next
     /// collection.
+    #[inline]
     pub fn set_pacing(&self, pacing: Pacing) {
         self.0.pacing.set(pacing);
     }
 
     /// Returns the total bytes allocated by the arena itself, used as the backing storage for `Gc`
     /// pointers.
+    #[inline]
     pub fn total_gc_allocation(&self) -> usize {
         self.0.total_gc_bytes.get()
     }
@@ -101,12 +107,14 @@ impl Metrics {
     ///
     /// A call to `Metrics::mark_external_allocation` will increase this count, and a call to
     /// `Metrics::mark_external_deallocation` will decrease it.
+    #[inline]
     pub fn total_external_allocation(&self) -> usize {
         self.0.total_external_bytes.get()
     }
 
     /// Returns the sum of `Metrics::total_gc_allocation()` and
     /// `Metrics::total_external_allocation()`.
+    #[inline]
     pub fn total_allocation(&self) -> usize {
         self.0
             .total_gc_bytes
@@ -118,6 +126,7 @@ impl Metrics {
     /// used to time incremental garbage collection based on the tuning parameters in the current
     /// `Pacing`. The allocation debt is measured in bytes, but will generally increase at a rate
     /// faster than that of allocation so that collection will always complete.
+    #[inline]
     pub fn allocation_debt(&self) -> f64 {
         // Estimate the amount of external memory that has been traced assuming that each Gc owns an
         // even share of the external memory.
@@ -137,6 +146,7 @@ impl Metrics {
     ///
     /// This affects the GC pacing, marking external bytes as allocated will trigger allocation
     /// debt.
+    #[inline]
     pub fn mark_external_allocation(&self, bytes: usize) {
         cell_update(&self.0.total_external_bytes, |b| b.saturating_add(bytes));
         cell_update(&self.0.external_debt, |d| {
@@ -153,6 +163,7 @@ impl Metrics {
     /// It is safe, but may result in unspecified behavior (such as very weird or non-existent gc
     /// pacing), if the amount of bytes marked for deallocation is greater than the number of bytes
     /// marked for allocation.
+    #[inline]
     pub fn mark_external_deallocation(&self, bytes: usize) {
         cell_update(&self.0.total_external_bytes, |b| b.saturating_sub(bytes));
         cell_update(&self.0.external_debt, |d| d - bytes as f64);
@@ -179,6 +190,7 @@ impl Metrics {
         self.0.wakeup_amount.set(wakeup_amount);
     }
 
+    #[inline]
     pub(crate) fn mark_gc_allocated(&self, bytes: usize) {
         cell_update(&self.0.total_gc_bytes, |b| b + bytes);
         cell_update(&self.0.total_gcs, |c| c + 1);
@@ -187,17 +199,20 @@ impl Metrics {
         });
     }
 
+    #[inline]
     pub(crate) fn mark_gc_traced(&self, bytes: usize) {
         cell_update(&self.0.traced_gcs, |c| c + 1);
         cell_update(&self.0.gc_debt, |d| d - bytes as f64);
     }
 
+    #[inline]
     pub(crate) fn mark_gc_deallocated(&self, bytes: usize) {
         cell_update(&self.0.total_gc_bytes, |b| b - bytes);
         cell_update(&self.0.total_gcs, |c| c - 1);
         cell_update(&self.0.gc_debt, |d| d - bytes as f64);
     }
 
+    #[inline]
     pub(crate) fn mark_gc_remembered(&self, bytes: usize) {
         cell_update(&self.0.remembered_gcs, |c| c + 1);
         cell_update(&self.0.remembered_gc_bytes, |b| b + bytes);
