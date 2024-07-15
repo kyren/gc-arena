dd7060f940d399c36d7f0e792a0a2670a1d7b3c5
Moulins <arthur.heuillard@orange.fr>
2024-07-15 03:13:04 +0200

    More method parity between `Gc` and `GcWeak`
    - Don't require `T: Sized` for `GcWeak::{from_ptr, cast}`
    - Add `GcWeak::erase`

--- src/gc_weak.rs
+++ src/gc_weak.rs
@@ -104,12 +104,10 @@ impl<'gc, T: ?Sized + 'gc> GcWeak<'gc, T> {
     pub fn as_ptr(self) -> *const T {
         Gc::as_ptr(self.inner)
     }
-}
 
-impl<'gc, T: 'gc> GcWeak<'gc, T> {
     /// Cast the internal pointer to a different type.
     ///
-    /// SAFETY:
+    /// # Safety
     /// It must be valid to dereference a `*mut U` that has come from casting a `*mut T`.
     #[inline]
     pub unsafe fn cast<U: 'gc>(this: GcWeak<'gc, T>) -> GcWeak<'gc, U> {
@@ -118,9 +116,21 @@ impl<'gc, T: 'gc> GcWeak<'gc, T> {
         }
     }
 
+    /// Cast a `GcWeak` to the unit type.
+    ///
+    /// This is exactly the same as `unsafe { GcWeak::cast::<()>(this) }`, but we can provide this
+    /// method safely because it is always safe to dereference a `*mut ()` that has come from
+    /// casting a `*mut T`.
+    #[inline]
+    pub fn erase(this: GcWeak<'gc, T>) -> GcWeak<'gc, ()> {
+        GcWeak {
+            inner: Gc::erase(this.inner),
+        }
+    }
+
     /// Retrieve a `GcWeak` from a raw pointer obtained from `GcWeak::as_ptr`
     ///
-    /// SAFETY:
+    /// # Safety
     /// The provided pointer must have been obtained from `GcWeak::as_ptr` or `Gc::as_ptr`, and
     /// the pointer must not have been *fully* collected yet (it may be a dropped but valid weak
     /// pointer).
